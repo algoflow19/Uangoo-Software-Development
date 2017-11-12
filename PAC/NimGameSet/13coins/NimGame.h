52875f9439348a05e2cdffa81bef53d25961bb01
Jace Belere <2957691307@qq.com>
2017-11-12 15:52:03 +0800

    missing '&' cause waste a lot of time!

---
+++ PAC/NimGameSet/13coins/NimGame.h
@@ -0,0 +1,49 @@
+#ifndef NIMGAME_H
+#define NIMGAME_H
+#include"vector.h"
+enum side { HUMAN, COMPUTER };
+
+struct Move {
+    int nTaken;
+};
+
+class NimGame
+{
+public:
+    NimGame();
+    ~NimGame();
+
+    void printInstructions();
+    void play();
+
+
+private:
+    /* State saver*/
+    int coinLeft;
+    side whosTurn;
+
+    /* Paly Fun number!*/
+
+    void initGame();
+    bool gameIsOver();
+    void displayGame();
+    side getCurrentPlayer();
+    Move getUserMove();
+    void displayMove(Move move);  // Only use for computer!
+    void makeMove(Move move); // Working in background.
+    void retractMove(Move move);
+    void switchTurn();
+    void announceResult();
+
+    /* Computer move maker*/
+
+    void generateMoveList(Vector<Move> &move);
+    Move getComputerMove();
+    Move findBestMove();
+    Move findBestMove(int depth, int &rating);
+    int evaluatePosition(int depth);
+    int evaluateStaticPosition();
+};
+
+
+#endif // NIMGAME_H
