fd8bf1e19f40513de5f56968cd5cdf51b6a178fe
Jace Belere <2957691307@qq.com>
2017-11-13 10:56:10 +0800

    Create main.cpp

---
+++ PAC/NimGameSet/3rows/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include<iomanip>
+#include"error.h"
+#include"vector.h"
+#include"NimGame.h"
+
+using namespace std;
+
+int main() {
+
+    NimGame game;
+    game.printInstructions();
+    game.play();
+
+    return 0;
+}
