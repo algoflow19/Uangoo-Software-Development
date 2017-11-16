d2c4eb7ad6dae6bc1aabdd8e26c03ebacd6f2e29
Jace Belere <2957691307@qq.com>
2017-11-16 12:48:57 +0800

    Update TokenScanner.h

--- PAC/tokenScanner/TokenScanner.h
+++ PAC/tokenScanner/TokenScanner.h
@@ -1,6 +1,6 @@
 #pragma once
 #include<string>
-
+#include<iostream>
 
 class TokenScanner
 {
@@ -11,27 +11,24 @@ public:
 	TokenScanner(std::string instr);
 	void setInput(std::string instr);
 
+	void setInput(std::istream & infile);
 	std::string nextToken();
 	void saveToken(std::string token);
 	bool hasMoreTokens();
-	
+
 	void setStringscannFlag();
 	void setWhiteSpaceSkipFlag();
 	void setNumberscannFlag();
 
 private:
-
+	
+	std::istream * infile;
 	std::string scannDefault();
-
 	char scannString();
-
 	std::string scannNumber();
-
 	void skipWhiteSpace();
-
 	std::string stackpop();
-
-	int start,pos;
+	int start, pos;
 
 	std::string ignoreSet;
 	std::string sourceStr;
@@ -44,5 +41,6 @@ private:
 	bool stringScannFlag;
 	bool numberScannFlag;
 
+	/* reflash the sourceStr */
+	void sourceStrReflash();
 };
-
