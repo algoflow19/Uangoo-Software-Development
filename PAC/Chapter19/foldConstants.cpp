e2f6c7589a44964c608306c68bc9b5c76b4c0849
develpoerkk <2957691307@qq.com>
2018-03-12 14:08:22 +0800

    This funtion is not ideal work!
    Signed-off-by: develpoerkk <2957691307@qq.com>

---
+++ PAC/Chapter19/foldConstants.cpp
@@ -0,0 +1,64 @@
+/*
+ * File: Interpreter.cpp
+ * ---------------------
+ * This program simulates the top level of an expression interpreter.  The
+ * program reads an expression, evaluates it, and then displays the result.
+ */
+
+#include <iostream>
+#include <string>
+#include "error.h"
+#include "exp.h"
+#include "parser.h"
+#include "tokenscanner.h"
+#include"console.h"
+using namespace std;
+
+bool expMatch(Expression *e1,Expression *e2);
+#define comp
+
+void BolandOutput(Expression* exp);
+Expression* constantMerge(Expression* src);
+
+
+int main() {
+   EvaluationContext context;
+   TokenScanner scanner;
+   Expression *exp;
+   scanner.ignoreWhitespace();
+   scanner.scanNumbers();
+   while(true){
+      try {
+         string line;
+         cout << "=> ";
+         getline(cin, line);
+         if (line == "quit") break;
+         scanner.setInput(line);
+         Expression *exp1 = parseExp(scanner);
+         cout<<constantMerge(exp1)->toString()<<endl;
+      } catch (ErrorException ex) {
+         cerr << "Error: " << ex.getMessage() << endl;
+      }
+   }
+
+   return 0;
+}
+
+Expression* constantMerge(Expression* src){
+    ExpressionType type=src->getType();
+    if(type==CONSTANT) return new ConstantExp(src->getConstantValue());
+    if(type==IDENTIFIER) return new IdentifierExp(src->getIdentifierName());
+    // Ignore SignleOpExp to reduce complex
+    if(type==COMPOUND){
+        Expression *rhs,*lhs;
+        rhs=constantMerge(src->getRHS());
+        lhs=constantMerge(src->getLHS());
+        EvaluationContext context;
+        if(rhs->getType()==CONSTANT&&lhs->getType()==CONSTANT)
+            return new ConstantExp(src->eval(context));
+        else
+            return new CompoundExp(src->getOperator(),lhs,rhs);
+    }
+    error("Unknow type exp!");
+    return NULL;
+}
